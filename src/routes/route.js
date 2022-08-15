f03870264d5d665e0a0ef1a696d715750cfff4e0
Prabhas Prasad <prabhasprasad106@gmail.com>
2022-08-16 00:55:08 +0530

    hello

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const bookModel= require("../models/bookModel.js")
+const bookModel= require("../models/bookModel")
 const bookController= require("../controllers/bookController")
 
 router.get("/test-me", function (req, res) {
