b13ef25202136561e9971a87a12b6454cc466e46
Prabhas Prasad <prabhasprasad106@gmail.com>
2022-09-03 00:31:30 +0530

    hi

---
+++ src/controllers/controller.js
@@ -0,0 +1,37 @@
+const axios = require('axios')
+// -----------------------Get All Memes---------------------
+const getAllMemes = async (req, res) => {
+  try {
+    const options = {
+      method: 'get',
+      url: 'https://api.imgflip.com/get_memes'
+    }
+    const result = await axios(options);
+    console.log(options);
+    const data = result.data
+    res.status(200).send({ msg: data, status: true })
+  }
+  catch (err) {
+    res.status(500).send({ msg: err.message })
+  }
+}
+//-----------------------Create All Memes---------------------
+const createMeme = async (req, res) => {
+  try {
+    const template_id = req.query.template_id;
+    const text0 = req.query.text0
+    const text1 = req.query.text1
+    const username = req.query.username;
+    const password = req.query.password;
+    const options = {
+      method: 'post',
+      url: `https://api.imgflip.com/caption_image?template_id=${template_id}&text0=${text0}&text1=${text1}&username=${username}&password=${password}`
+    }
+    const result = await axios(options)
+    res.status(200).send({ msg: result.data, status: true })
+  }
+  catch (err) {
+    res.status(500).send({ error: err.message })
+  }
+}
+module.exports = { getAllMemes, createMeme }
--- src/routes/route.js
+++ src/routes/route.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const userController = require("../controllers/userController")
-const middleware = require('../middleware/commonMiddleware');
+const controller = require("../controllers/controller")
 
-router.post("/users", middleware.mid1,userController.createUser)
-router.post("/login",  middleware.mid1,userController.loginUser)
-router.get("/users/:userId", middleware.mid2, userController.getUserData)
-router.put("/users/:userId", middleware.mid1, middleware.mid2, userController.updateUser)
-router.delete("/users/:userId", middleware.mid1, middleware.mid2, userController.deleteUser)
-router.post("/users/:userId/post", middleware.mid1, middleware.mid2, userController.postMessage)
+router.get("/getAllMemes", controller.getAllMemes);
+router.post("/createMeme", controller.createMeme);
 
 module.exports = router;
